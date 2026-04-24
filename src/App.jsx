12272693639df62099ee3f6752d681e0392734b2
Lalechu Oneneobari <lalechuoneneobari4@gmail.com>
2026-04-24 15:05:34 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function Greeting() {
+     
+  return ( 
+    
+      <h1> hello word</h1>
+    
+  )
+}
+
+function App() {
+ 
+  return (
+    <>
+      <Greeting />
+    </>
+  )
+}
+
+export default App
